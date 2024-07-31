1bb83692ee8768334abf4bf749e6128d1c2b6ceb
ibrahim8545 <ibrahimmostafa122020@gmail.com>
2024-08-01 00:39:51 +0300

    add custom scroll view and refactor my code and home screen one finished

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,156 +10,180 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.only(
-          top: 60,
-          left: 26,
-          right: 26,
-         ),
-        child: Column(children: [
-          CustomAppBar(),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            children: [
-              Text(
-                'Categories',
-                style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                    fontFamily: 'Poppins'),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 60,
+                left: 26,
+                right: 26,
               ),
-              Spacer(),
-              Text(
-                'See more',
-                style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                    fontFamily: 'Poppins',
-                    color: Color(0xff4838D1)),
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          const SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                SizedBox(
-                  width: 30,
-                ),
-                Text(
-                  'Art',
-                  style: Styles.textStyle16,
-                ),
-                SizedBox(
-                  width: 30,
-                ),
-                Text(
-                  'Bussines',
-                  style: Styles.textStyle16,
-                ),
-                SizedBox(
-                  width: 30,
-                ),
-                Text(
-                  'Commedy',
-                  style: Styles.textStyle16,
-                ),
-                SizedBox(
-                  width: 30,
-                ),
-                Text(
-                  'Dramma',
-                  style: Styles.textStyle16,
-                ),
-              ],
+              child: Column(
+                children: [
+                  const CustomAppBar(),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      const Text(
+                        'Categories',
+                        style: Styles.textStyle16,
+                      ),
+                      const Spacer(),
+                      Text('See more',
+                          style: Styles.textStyle14.copyWith(
+                              color: const Color(0xff4838D1),
+                              fontWeight: FontWeight.w500))
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                ],
+              ),
             ),
           ),
-          SizedBox(
-            height: 50,
-          ),
-          Row(
-            children: [
-              Text(
-                'Recommended For You',
-                style: Styles.textStyle16.copyWith(fontWeight: FontWeight.w600),
-              ),
-              const Spacer(),
-              Text(
-                'See more',
-                style: Styles.textStyle16.copyWith(
-                    fontWeight: FontWeight.w500, color:const  Color(0xff4838D1)),
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 20
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                Image.asset('assets/images/film2.png',height: 300,),
-                const SizedBox(
-                  width: 20,
+          const SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 26),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Text(
+                      'Art',
+                      style: Styles.textStyle16,
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Text(
+                      'Business',
+                      style: Styles.textStyle16,
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Text(
+                      'Comedy',
+                      style: Styles.textStyle16,
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Text(
+                      'Dramama',
+                      style: Styles.textStyle16,
+                    ),
+                  ],
                 ),
-                Image.asset('assets/images/film.png',),
-              ],
+              ),
             ),
           ),
-          const SizedBox(
-            height: 50
+          const SliverToBoxAdapter(
+            child: SizedBox(height: 50),
           ),
-          const Row(
-            children: [
-              Text(
-                'Best Seller',
-                style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                    fontFamily: 'Poppins'),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 26),
+              child: Row(
+                children: [
+                  Text(
+                    'Recommended For You',
+                    style: Styles.textStyle16
+                        .copyWith(fontWeight: FontWeight.w600),
+                  ),
+                  const Spacer(),
+                  Text(
+                    'See more',
+                    style: Styles.textStyle16.copyWith(
+                        fontWeight: FontWeight.w500,
+                        color: const Color(0xff4838D1)),
+                  )
+                ],
               ),
-              Spacer(),
-              Text(
-                'See more',
-                style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                    fontFamily: 'Poppins',
-                    color: Color(0xff4838D1)),
-              )
-            ],
+            ),
           ),
-          const SizedBox(
-            height: 10
+          const SliverToBoxAdapter(
+            child: SizedBox(height: 20),
           ),
-          Expanded(
-            
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 26),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Image.asset('assets/images/film2.png', height: 300),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    Image.asset('assets/images/film.png'),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const SliverToBoxAdapter(
+            child: SizedBox(height: 50),
+          ),
+          const SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 26),
+              child: Row(
+                children: [
+                  Text(
+                    'Best Seller',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'Poppins'),
+                  ),
+                  Spacer(),
+                  Text(
+                    'See more',
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'Poppins',
+                        color: Color(0xff4838D1)),
+                  )
+                ],
+              ),
+            ),
+          ),
+          const SliverToBoxAdapter(
+            child: SizedBox(height: 10),
+          ),
+          SliverFillRemaining(
             child: ListView.separated(
-              padding: EdgeInsets.zero,
-              separatorBuilder: (context, index) => SizedBox(height: 5,),
+              padding: const EdgeInsets.symmetric(horizontal: 26),
+              physics: const NeverScrollableScrollPhysics(),
+              separatorBuilder: (context, index) => const SizedBox(
+                height: 5,
+              ),
               itemBuilder: (context, index) {
-              return FilmItem();
-            },
-            itemCount: 100,
+                return const FilmItem();
+              },
+              itemCount: 100,
             ),
           ),
-        
-          
-          
-        ]),
+        ],
       ),
       bottomNavigationBar: Container(
         padding: EdgeInsets.zero,
         child: BottomNavigationBar(
-          items: [
-            const BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
             BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-            BottomNavigationBarItem(icon: ImageIcon(AssetImage('assets/images/lib.png')), label: 'Library'),
+            BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage('assets/images/lib.png')),
+                label: 'Library'),
           ],
         ),
       ),
